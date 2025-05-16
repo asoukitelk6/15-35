05b4b3128125c19e1b87b212429c928e2b8ddf7b
asoukitelk6 <asoukitelk6@gmail.com>
2025-05-16 22:47:10 +0400

    основная задача выполнена

--- main.py
+++ main.py
@@ -4,3 +4,39 @@
 Реализуй функцию для добавления задач, отметки выполненных задач и вывода списка текущих
 (не выполненных) задач.'''
 
+class Task():
+    def __init__(self):
+        self.tasks = []
+
+    def new_task (self, opis, time, status=False):#Реализуй функцию для добавления задач,
+        task={'opis':opis,'time':time,'status':status}
+        self.tasks.append(task)
+        print(f'задача "{opis}" добавленна')
+
+    def otmetka (self, opis):#отметки выполненных задач
+        for task in self.tasks:
+            if task['opis'] == opis:
+                task['status'] = True
+                print(f'Задача с описанием :"{opis}" выполненна')
+                break
+        else:
+            print(f'Задача "{opis}" ненайдена')
+
+    def itog_task (self):#вывода списка текущих (не выполненных) задач.
+        print(f'Активные задачи:')
+        n = 1
+        for task in self.tasks:
+            if task['status'] == False:
+                print(f'{n}) {task["opis"]} нужно выполнить до {task["time"]}')
+                n += 1
+
+manager = Task()
+manager.new_task("Купить молоко", "2025-04-10")
+manager.new_task("Купить 1", "2025-05-10")
+
+manager.itog_task()
+
+manager.otmetka("Купить1")
+manager.otmetka("Купить 1")
+
+manager.itog_task()
